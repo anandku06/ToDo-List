75e02a31e2a794bd626e6e04be2ac6f02198eaa6
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-03 00:12:50 +0530

    Adding and Deleting the Task
    Refined the code and Done with the Adding and completion of Tasks with deleting all the task

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
 const todoInput = document.getElementById("todoInput");
 
-const todolist = document.getElementById("todoList");
+const todoList = document.querySelector(".todoList");
 const todoCount = document.getElementById("todoCount");
 const addButton = document.querySelector(".btn");
 const deleteButton = document.getElementById("deleteBtn");
@@ -23,8 +23,8 @@ function addTask() {
   const newTask = todoInput.value.trim();
   if (newTask !== "") {
     todo.push({
-        text: newTask, 
-        disabled: false
+      text: newTask,
+      disabled: false,
     });
     saveToLocalStorage();
     todoInput.value = "";
@@ -32,12 +32,41 @@ function addTask() {
   }
 }
 
-function saveToLocalStorage(){
-    localStorage.setItem("todo", JSON.stringify(todo))
+function saveToLocalStorage() {
+  localStorage.setItem("todo", JSON.stringify(todo));
 }
 
-function deleteAllTasks(){
-
+function deleteAllTasks() {
+    todo = [];
+    saveToLocalStorage()
+    displayTasks()
 }
 
-function displayTasks(){}
+function displayTasks() {
+  todoList.innerHTML = "";
+  todo.forEach((item, index) => {
+    const p = document.createElement("p");
+    p.innerHTML = `
+        <div class="todo-container">
+          <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
+      item.disabled ? "checked" : ""
+    }>
+          <p id="todo-${index}" class="${
+      item.disabled ? "disabled" : ""
+    }" onclick="editTask(${index})">${item.text}</p>
+        </div>
+      `;
+    p.querySelector(".todo-checkbox").addEventListener("change", () =>
+      toggleTask(index)
+    );
+    todoList.appendChild(p);
+  });
+  todoCount.textContent = todo.length;
+}
+
+function toggleTask(index) {
+    todo[index].disabled = !todo[index].disabled
+    saveToLocalStorage()
+    displayTasks()
+}
+
