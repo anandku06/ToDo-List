4ad06982befe39959655776a3ad608b825d77668
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-03 00:37:34 +0530

    Update script.js
    Done with Editing the task functionality

--- script.js
+++ script.js
@@ -70,3 +70,23 @@ function toggleTask(index) {
     displayTasks()
 }
 
+function editTask(index){
+    const todoItem = document.getElementById(`todo-${index}`)
+    const existingText = todo[index].text
+    const inputElement = document.createElement("input")
+
+    inputElement.value = existingText
+    todoItem.replaceWith(inputElement)
+
+    inputElement.focus()
+
+    inputElement.addEventListener("blur", function() {
+        const updatedText = inputElement.value.trim();
+        if(updatedText)
+        {
+            todo[index].text = updatedText
+            saveToLocalStorage()
+        }
+        displayTasks()
+    })
+}
